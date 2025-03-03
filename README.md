b2f55d000af6db97bdf395afff58f5e2fe342890
Rorry <rabrenne19@gmail.com>
2025-03-03 18:15:06 -0500

    checking in script that works for first half of process

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 
 "Lenski-esque AI competition trials with validated assertion databases"
 
+## Setup and install
+Everything should be done in the XGDAG folder.
+
+    cd XGDAG
+    ENVNAME=ENVxgdag && python -m venv $ENVNAME && source $ENVNAME/bin/activate
+    pip install scikit-learn torch_geometric rdkit seaborn torch
+
+## Data
+
+The data generated and provided in this repository are based on PPI data from [BioGRID](https://thebiogrid.org/) and Gene-Disease Associations from [DisGeNET](https://www.disgenet.org/). The original data can be dowloaded from the related websites. Part of the analysis relies on the set of all disease associations from DisGeNET. Given the size of this file, it needs to be manually downloaded from [here](https://drive.google.com/file/d/12cyI6ds0mKQI9mcRgaf0_9v8KDZHWpQR/view?usp=sharing) and placed in the ```Datasets``` folder.
+
+Using the aformentioned data we built graphs available for use in the ```Graphs``` folder. The script ```CreateGraph.py``` was used for this purpose.
+
+## Run
+
+    CUDA_VISIBLE_DEVICES=0 python TrainerScript.py 
+
 # Aim
 
 The aim of the project is to examine how graph neural networks learn and provide valuable insights from a network-type of data. We plan to feed a graph neural network (GNN) with a large biological network (e.g., DisGeNET, PrimeKG, SPOKE, petagraph). Then change the network architecture and compete both networks agains each other to examine how GNN evolve.
---
+++ XGDAG/ComputeRankingScript.py
@@ -0,0 +1,148 @@
+from GNNTrain import predict_from_saved_model
+from CreateDatasetv2 import get_dataset_from_graph
+from Paths import PATH_TO_GRAPHS, PATH_TO_RANKINGS
+from GDARanking import predict_candidate_genes
+import CreateDatasetv2_binary_diamond
+
+import os
+import sys
+from time import perf_counter
+
+# disease_Ids = ['C0006142',  'C0009402', 'C0023893', \
+#                'C0036341',  'C0376358', 'C3714756', \
+#                'C0860207',  'C0011581', 'C0005586', \
+#                'C0001973']
+
+disease_Ids = ['C0006142',  'C0009402', 'C0023893']
+
+methods = ['gnnexplainer',  'gnnexplainer_only',\
+           'graphsvx',      'graphsvx_only',    \
+           'subgraphx',     'subgraphx_only']
+
+def check_args(args):
+    if len(args) < 3:
+        if len(args) == 2 and (args[1] == '-h' or args[1] == '--help'):
+            print('\n\n[Usage]: python ComputeRankingScript.py disease_id explainability_method num_cores\n')
+            print('Available diseases:\n', disease_Ids, '\nType all to compute the ranking for all the available diseases\n')
+            print('Available methods:\n', methods, '\nType all to compute the ranking for all the available methods\n')
+            print('num_cores: define the number of cores to parallelize the explainability method\n\n')
+        else:
+            print('\n\n[ERR] Wrong input parameters: use -h or --help to print the usage\n\n')
+        return -1
+
+    disease_Id = args[1]
+    METHOD = args[2]
+    num_cpus = int(args[3])
+
+    if disease_Id not in disease_Ids and disease_Id.lower() != 'all':
+        print('\n[ERR] Disease ID', disease_Id, 'not present in the database\n')
+        return -1
+
+    if METHOD not in methods and METHOD.lower() != 'all':
+        print('\n[ERR] Method', METHOD, 'not available\n')
+        return -1
+    
+    if num_cpus < 1:
+        print('\n[ERR]', num_cpus,'is an nvalid number of cores\n')
+        return -1
+    
+    return disease_Id, METHOD, num_cpus
+
+def ranking(disease_Id, METHOD, num_cpus, filename, modality='multiclass'):
+
+    model_name  = 'GraphSAGE_' + disease_Id + '_diamond'
+    graph_path  = PATH_TO_GRAPHS + 'grafo_diamond_nedbit_' + disease_Id + '.gml'
+    classes     = ['P', 'LP', 'WN', 'LN', 'RN']
+
+    if modality == 'binary':
+        model_name += '_binary'
+        classes = ['P', 'U']
+        dataset, G = CreateDatasetv2_binary_diamond.get_dataset_from_graph(graph_path, disease_Id, quartile=False)
+    else:
+        dataset, G = get_dataset_from_graph(graph_path, disease_Id, quartile=False, from_diamond=True)
+
+    model_name += '_40000_0_0005'
+
+    preds, probs, model = predict_from_saved_model(model_name, dataset, classes, save_to_file=False)
+
+    ranking = predict_candidate_genes(model,
+                                    dataset,
+                                    preds,
+                                    explainability_method=METHOD,
+                                    disease_Id=disease_Id,
+                                    explanation_nodes_ratio=1,
+                                    masks_for_seed=10,
+                                    num_hops=1,
+                                    G=G,
+                                    num_pos="all",
+                                    num_workers=num_cpus)
+
+    print('[+] Saving ranking to file', filename, end='...')
+
+    with open(filename, 'w') as f:
+        for line in ranking:
+            f.write(line + '\n')
+
+    print('ok')
+
+def sanitized_input(prompt, accepted_values):
+    res = input(prompt).strip().lower()
+    if res not in accepted_values:
+        return sanitized_input(prompt, accepted_values)
+    return res
+
+if __name__ == '__main__':
+    t_start = perf_counter()
+
+    args = check_args(sys.argv)
+
+    if args == -1:
+        sys.exit()
+    
+    disease_Id = args[0]
+    METHOD = args[1]
+    num_cpus = args[2]
+
+    # modality = 'multiclass'
+    # if '_only' in METHOD:
+    #     modality = 'binary'
+
+    if disease_Id != 'all':
+        disease_Ids = [disease_Id]
+    
+    if METHOD != 'all':
+        methods = [METHOD]
+    
+    print('[i] Computing the ranking for', disease_Ids, '(', len(disease_Ids), ')', 'disease(s).')
+    
+    for disease_Id in disease_Ids:
+
+        for METHOD in methods:
+            print('[i] Starting', disease_Id, 'with method', METHOD)
+
+            filename = PATH_TO_RANKINGS + disease_Id + '_all_positives_diamond_'
+
+            modality = 'multiclass'
+            if '_only' in METHOD:
+                modality = 'binary'
+            
+            if modality == 'multiclass':
+                filename += 'xgdag_' + METHOD.lower() + '.txt'
+            else:
+                filename += METHOD.lower().replace("_only", "") + '.txt'
+
+            res = ''
+            if os.path.exists(filename):
+                res = sanitized_input('[+] A raking for disease ' + disease_Id + \
+                    ' has already been computed with ' + METHOD + \
+                    '. Do you want to overwrite the old ranking? (y|n) ', ['y', 'n'])
+            
+            if res == 'n':
+                print('[i] Skipping disease', disease_Id, 'with method', METHOD)
+                continue
+            else:
+                # Compute the ranking
+                ranking(disease_Id, METHOD, num_cpus, filename, modality)
+
+    t_end = perf_counter()
+    print('[i] Elapsed time:', round(t_end - t_start, 3))
