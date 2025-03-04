8f53eb08e02fe82a8473e4fd7279f8c5ea4732b2
Nanami Kubota <86127860+NanamiKubota@users.noreply.github.com>
2025-03-04 13:55:42 -0500

    update data preprocessing scripts
    Reran jupyter file to make sure scripts are up-to-date.
    python v3.10.16
    pecanpy v2.0.9

--- geneDRAGNN/data_preprocessing/import_gdc.py
+++ geneDRAGNN/data_preprocessing/import_gdc.py
@@ -5,17 +5,27 @@ def ImportGDC():
 
     featureNameColumns = ['# SSM Affected Cases in Cohort', '# CNV Gain', '# CNV Loss']
     for i in featureNameColumns:
-        gdc[['1', '2', '3', '4']] = gdc[i].replace({',':''}, regex=True).str.split(' ', 3, expand=True)
-        gdc[i] = gdc['1'].astype(float)/gdc['3'].astype(float)
-        gdc = gdc.drop(['1', '2', '3', '4'], axis=1)
+        # Extract numerator and denominator
+        gdc[[i + '_num', i + '_den']] = gdc[i].str.replace(',', '', regex=True).str.extract(r'(\d+) / (\d+)')
+        # Convert to float and compute ratio
+        gdc[i] = gdc[i + '_num'].astype(float) / gdc[i + '_den'].astype(float)
+        # Drop temporary columns
+        gdc.drop(columns=[i + '_num', i + '_den'], inplace=True)
 
+    # Drop unnecessary columns
     gdc.drop(['Symbol', 'Name', 'Cytoband', 'Type', 'Annotations', 'Survival'], axis=1, inplace=True)
 
-    gdc[['1', '2', '3']] = gdc['# SSM Affected Cases Across the GDC'].replace({',':''}, regex=True).str.split(' ', 2, expand=True)
-    gdc['# SSM Affected Cases Across the GDC'] = gdc['1'].astype(float)/gdc['3'].astype(float)
-    gdc = gdc.drop(['1', '2', '3'], axis=1)
+    # Process '# SSM Affected Cases Across the GDC'
+    gdc[['ssm_num', 'ssm_den']] = gdc['# SSM Affected Cases Across the GDC'].str.replace(',', '', regex=True).str.extract(r'(\d+) / (\d+)')
+    gdc['# SSM Affected Cases Across the GDC'] = gdc['ssm_num'].astype(float) / gdc['ssm_den'].astype(float)
+    gdc.drop(columns=['ssm_num', 'ssm_den'], inplace=True)
 
-    gdc = gdc.rename({'# SSM Affected Cases in Cohort': 'nih_ssm_in_cohort', '# SSM Affected Cases Across the GDC':'nih_ssm_across_gdc',
-        '# CNV Gain':'nih_cnv_gain', '# CNV Loss':'nih_cnv_loss', 'Gene ID':'ensembl', '# Mutations':'nih_tot_mutations'}, axis=1)
+    # Rename columns
+    gdc = gdc.rename({'# SSM Affected Cases in Cohort': 'nih_ssm_in_cohort', 
+                      '# SSM Affected Cases Across the GDC': 'nih_ssm_across_gdc',
+                      '# CNV Gain': 'nih_cnv_gain', 
+                      '# CNV Loss': 'nih_cnv_loss', 
+                      'Gene ID': 'ensembl', 
+                      '# Mutations': 'nih_tot_mutations'}, axis=1)
 
     return gdc
