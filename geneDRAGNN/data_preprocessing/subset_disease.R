290f5b2be3bc4417c21043b408500b456236634e
Nanami Kubota <86127860+NanamiKubota@users.noreply.github.com>
2025-03-04 15:24:44 -0500

    update subset R script

--- geneDRAGNN/data_preprocessing/subset_disease.R
+++ geneDRAGNN/data_preprocessing/subset_disease.R
@@ -1,7 +1,7 @@
 
 setwd("/Users/kubotan/Documents/github/LenskAI/geneDRAGNN")
 
-all_disease <- read.delim("./data/all_gene_disease_associations.tsv")
+all_disease <- read.delim("./data_subset/all_gene_disease_associations.tsv")
 
 library(tidyverse)
 library(stringr)
@@ -12,5 +12,5 @@ bone_disease <- all_disease %>%
 brain_disease <- all_disease %>%
   dplyr::filter(str_detect(diseaseName, "glio"))
 
-write.csv(bone_disease, "./data/bone_disease_associations.csv")
-write.csv(brain_disease, "./data/brain_disease_associations.csv")
+write.csv(bone_disease, "./data/bone_disease_associations.csv", row.names = FALSE)
+write.csv(brain_disease, "./data/brain_disease_associations.csv", row.names = FALSE)
