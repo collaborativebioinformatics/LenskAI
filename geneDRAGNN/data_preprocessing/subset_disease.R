9049682953a872e962aab1e70566305c6efbffbd
Nanami Kubota <86127860+NanamiKubota@users.noreply.github.com>
2025-03-05 13:08:52 -0500

    updated subsetting of data for preprocessing

--- geneDRAGNN/data_preprocessing/subset_disease.R
+++ geneDRAGNN/data_preprocessing/subset_disease.R
@@ -1,5 +1,5 @@
 
-setwd("/Users/kubotan/Documents/github/LenskAI/geneDRAGNN")
+setwd("/Users/kubotan/Documents/github/LenskAI/geneDRAGNN/data_preprocessing/brain/data_subset")
 
 all_disease <- read.delim("./data_subset/all_gene_disease_associations.tsv")
 
@@ -7,10 +7,16 @@ library(tidyverse)
 library(stringr)
 
 bone_disease <- all_disease %>%
-  dplyr::filter(str_detect(diseaseName, "bone"))
+  dplyr::filter(str_detect(diseaseName, "bone"))%>%
+  select(geneSymbol, EI, score) %>%
+  rename(Gene = geneSymbol) %>%
+  distinct()
 
 brain_disease <- all_disease %>%
-  dplyr::filter(str_detect(diseaseName, "glio"))
+  dplyr::filter(str_detect(diseaseName, "glio")) %>%
+  select(geneSymbol, EI, score) %>%
+  rename(Gene = geneSymbol) %>%
+  distinct()
 
-write.csv(bone_disease, "./data/bone_disease_associations.csv", row.names = FALSE)
-write.csv(brain_disease, "./data/brain_disease_associations.csv", row.names = FALSE)
+write.csv(bone_disease, "./bone_disease_associations.csv", row.names = FALSE)
+write.csv(brain_disease, "./brain_disease_associations.csv", row.names = FALSE)
